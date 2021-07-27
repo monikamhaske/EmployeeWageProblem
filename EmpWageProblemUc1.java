079bf4e730c6b9bd5043185a312b17ae63c7bf10
monika <monikamhaske46@gmail.com>
2021-07-27 22:36:17 +0530

    Employee Wage Practice Problem

---
+++ EmpWageProblemUc1.java
@@ -0,0 +1,21 @@
+package Test;
+
+public class EmpWageProblemUc1 
+{
+public static void main(String[]args)
+{
+	//constants
+	int IS_FULL_TIME=1;
+	//Computation
+	double empCheck= Math.floor(Math.random()*10)%2;
+	if(empCheck==IS_FULL_TIME)
+	{
+		System.out.println("Employee is Present");
+	}
+	else
+	{
+		System.out.println("Employee is Absent");
+	}
+		
+}
+}
